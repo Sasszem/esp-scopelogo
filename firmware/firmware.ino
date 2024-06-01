eaa5f05c8c377fd09a095716deee413ca542e153
Sasszem <barath.laszlo.szolnok@gmail.com>
2024-06-01 22:42:50 +0200

    run clang-format

--- firmware/firmware.ino
+++ firmware/firmware.ino
@@ -1,25 +1,25 @@
-#include <driver/dac.h>
 #include "settings.h"
+#include <driver/dac.h>
 /*
 OTA imports and OTA task code
 */
 #ifdef ENABLE_OTA
-  #include <WiFi.h>
-  #include <ESPmDNS.h>
-  #include <WiFiUdp.h>
-  #include <ArduinoOTA.h>
+#include <ArduinoOTA.h>
+#include <ESPmDNS.h>
+#include <WiFi.h>
+#include <WiFiUdp.h>
 
-  TaskHandle_t OTATask;
+TaskHandle_t OTATask;
 
-  void OTATaskCode( void * parameter) {
-    WiFi.mode(WIFI_AP);
-    WiFi.softAP(OTA_SSID, OTA_PASS);
-    ArduinoOTA.begin();
+void OTATaskCode(void *parameter) {
+  WiFi.mode(WIFI_AP);
+  WiFi.softAP(OTA_SSID, OTA_PASS);
+  ArduinoOTA.begin();
 
-    for(;;) {
-      ArduinoOTA.handle();
-    }
+  for (;;) {
+    ArduinoOTA.handle();
   }
+}
 #endif
 
 extern char data[];
@@ -29,7 +29,6 @@ extern int count;
 
 int selected_index = 0;
 
-
 void setup() {
   // put your setup code here, to run once:
   dac_output_enable(DAC_CHANNEL_1);
@@ -37,35 +36,33 @@ void setup() {
   pinMode(BUTTON_PIN, INPUT_PULLUP);
   pinMode(LED_BUILTIN, OUTPUT);
 
-  #ifndef OTA
-    // NO OTA, just light up the LED
+#ifndef OTA
+  // NO OTA, just light up the LED
+  digitalWrite(LED_BUILTIN, LOW);
+#else
+  // OTA - diable led, activate OTA if needed
+  digitalWrite(LED_BUILTIN, HIGH);
+
+  delay(100);
+
+  if (digitalRead(BUTTON_PIN) == LOW) {
     digitalWrite(LED_BUILTIN, LOW);
-  #else
-    // OTA - diable led, activate OTA if needed
-    digitalWrite(LED_BUILTIN, HIGH);
-
-    delay(100);
-
-
-    if (digitalRead(BUTTON_PIN) == LOW) {
-      digitalWrite(LED_BUILTIN, LOW);
-      xTaskCreatePinnedToCore(
-            OTATaskCode, /* Function to implement the task */
-            "OTA task", /* Name of the task */
-            10000,  /* Stack size in words */
-            NULL,  /* Task input parameter */
-            0,  /* Priority of the task */
-            &OTATask,  /* Task handle. */
-            0); /* Core where the task should run */
-    }
-  #endif
+    xTaskCreatePinnedToCore(OTATaskCode, /* Function to implement the task */
+                            "OTA task",  /* Name of the task */
+                            10000,       /* Stack size in words */
+                            NULL,        /* Task input parameter */
+                            0,           /* Priority of the task */
+                            &OTATask,    /* Task handle. */
+                            0);          /* Core where the task should run */
+  }
+#endif
 }
 
 int handle_button() {
   static int last_change;
   static bool last_value;
   int new_value = digitalRead(BUTTON_PIN);
-  if (new_value != last_value && (millis()-last_change) > 100) {
+  if (new_value != last_value && (millis() - last_change) > 100) {
     last_value = new_value;
     last_change = millis();
     if (!new_value) {
@@ -79,14 +76,15 @@ void loop() {
   // put your main code here, to run repeatedly:
   // draw a circle
   int limit = sizes[selected_index];
-  char* data_start = &data[starts[selected_index] * 2];
-  for (int i = 0; i<limit;i++) {
-    dac_output_voltage(DAC_CHANNEL_1, data_start[2*i]);
-    dac_output_voltage(DAC_CHANNEL_2, data_start[2*i+1]);
+  char *data_start = &data[starts[selected_index] * 2];
+
+  for (int i = 0; i < limit; i++) {
+    dac_output_voltage(DAC_CHANNEL_1, data_start[2 * i]);
+    dac_output_voltage(DAC_CHANNEL_2, data_start[2 * i + 1]);
     delayMicroseconds(1); // nagy hogy lássuk
   }
-  
-  if (handle_button())
+
+  if (handle_button()) {
     selected_index = (selected_index + 1) % count;
-  
+  }
 }
--- firmware/settings.h
+++ firmware/settings.h
@@ -1,6 +1,5 @@
 const int BUTTON_PIN = 13;
 
-
 #define ENABLE_OTA false
-constexpr char* OTA_SSID = "Beta-source";
-constexpr char* OTA_PASS = "ferdinandbraun";
+constexpr char *OTA_SSID = "Beta-source";
+constexpr char *OTA_PASS = "ferdinandbraun";

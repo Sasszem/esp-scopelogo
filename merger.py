9852a70b1ba918ab4aaad721f43e63c7d5889874
Sasszem <barath.laszlo.szolnok@gmail.com>
2024-06-01 22:18:16 +0200

    Rework merger script

--- merger.py
+++ merger.py
@@ -1,23 +1,20 @@
 #!/bin/env python3
 import pickle
-
 import sys
 
-images = []
-
-for file in sys.argv[1:]:
-    with open(file, "rb") as f:
-        images.append(pickle.load(f))
 
 index = 0
 starts = []
 sizes = []
 data = []
-for im in images:
+for file in sys.argv[1:]:
+    with open(file, "rb") as f:
+        image = pickle.load(f)
     starts.append(index)
-    sizes.append(len(im)//2)
+    sizes.append(len(image)//2)
     index += sizes[-1]
-    data.extend(im)
+    data.extend(image)
+    print(f"'{file}' - {len(image)//2} samples")
 
 def to_array(ar):
     return "{" + ", ".join(str(i) for i in ar) + "}"
@@ -26,4 +23,4 @@ with open('shape.c', 'w') as f:
     f.write(f"char data[] = {to_array(data)};\n")
     f.write(f"long int starts[] = {to_array(starts)};\n")
     f.write(f"long int sizes[] = {to_array(sizes)};\n")
-    f.write(f"int count = {len(images)};\n")
+    f.write(f"int count = {len(sys.argv[1:])};\n")
